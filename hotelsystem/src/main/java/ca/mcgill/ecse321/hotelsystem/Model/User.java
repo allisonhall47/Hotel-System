db7c6010fbde454ba8f8d6b83b4d1fe74d78cba6
YassineMimet1 <yassine.mimet@mail.mcgill.ca>
2023-10-02 15:24:54 -0400

    added first model classes

---
+++ hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/Model/User.java
@@ -0,0 +1,48 @@
+package ca.mcgill.ecse321.hotelsystem.Model;
+
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class User {
+    @Id
+    @GeneratedValue
+    private String email;
+
+    private String name;
+    @OneToOne
+    private Account account;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+}
