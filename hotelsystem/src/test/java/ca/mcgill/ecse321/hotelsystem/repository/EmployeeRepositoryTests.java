9fdfe799315fdb9fc5d27b0aba3ea51a37fae8ec
souhailelhayani <souhail.200308@gmail.com>
2023-10-03 16:38:43 -0400

    tem commit

--- hotelsystem/src/test/java/ca/mcgill/ecse321/hotelsystem/repository/EmployeeRepositoryTests.java
+++ hotelsystem/src/test/java/ca/mcgill/ecse321/hotelsystem/repository/EmployeeRepositoryTests.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-public class PersonRepositoryTests {
+public class EmployeeRepositoryTests {
     @Autowired
     private EmployeeRepository employeeRepository;
 
