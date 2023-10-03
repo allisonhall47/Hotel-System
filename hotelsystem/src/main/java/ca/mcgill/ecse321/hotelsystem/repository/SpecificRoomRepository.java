430a20e4eea2a66898d4ea1512171c313dfc9083
YassineMimet1 <yassine.mimet@mail.mcgill.ca>
2023-10-03 15:24:01 -0400

    SpecificRoom repository updated

---
+++ hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/repository/SpecificRoomRepository.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.hotelsystem.repository;
+
+import ca.mcgill.ecse321.hotelsystem.Model.SpecificRoom;
+import ca.mcgill.ecse321.hotelsystem.Model.ViewType;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface SpecificRoomRepository extends CrudRepository<SpecificRoom, Integer> {
+    SpecificRoom findSpecificRoomByNumber(int number);
+    List<SpecificRoom> getSpecificRoomsByView(ViewType view);
+
+    void deleteByNumber(int number);
+}
