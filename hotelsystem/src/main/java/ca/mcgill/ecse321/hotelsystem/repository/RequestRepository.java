91a0794c09d5a7dfe658dd2bd4158356e630f63c
PatrickRic <patrick.ric02@gmail.com>
2023-10-03 15:24:16 -0400

    Create RequestRepository.java

---
+++ hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/repository/RequestRepository.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.hotelsystem.repository;
+
+import ca.mcgill.ecse321.hotelsystem.Model.Request;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface RequestRepository extends CrudRepository<Request, Integer> {
+    Request findRequestByRequestId(int id);
+    List<Request> findRequestByReservation_ReservationID(int id);
+    void deleteRequestByRequestId(int id);
+    void deleteRequestsByReservation_ReservationID(int id);
+}
