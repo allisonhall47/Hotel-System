163281c14bf959c18fe1b4595513b98aed4f1afd
Allison Hall <91996222+allisonhall47@users.noreply.github.com>
2023-10-05 12:36:15 -0400

    Update RoomRepository.java

--- hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/repository/RoomRepository.java
+++ hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/repository/RoomRepository.java
@@ -3,6 +3,6 @@ package ca.mcgill.ecse321.hotelsystem.repository;
 import ca.mcgill.ecse321.hotelsystem.Model.Room;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RoomRepository extends CrudRepository<Room, String>{
-    Room findRoomByType (String type);
+public interface RoomRepository extends CrudRepository<Room, String> {
+    Room findRoomByType(String type);
 }
