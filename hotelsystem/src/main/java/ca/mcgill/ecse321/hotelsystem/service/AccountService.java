9783b8025da9a4cf698e0fe4129cd4fdf19cc840
allisonhall47 <allison.hall@mail.mcgill.ca>
2023-10-22 20:59:25 -0400

    start service classes

--- hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/service/AccountService.java
+++ hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/service/AccountService.java
@@ -2,40 +2,46 @@ package ca.mcgill.ecse321.hotelsystem.service;
 
 import ca.mcgill.ecse321.hotelsystem.Model.Account;
 import ca.mcgill.ecse321.hotelsystem.repository.AccountRepository;
-import ca.mcgill.ecse321.hotelsystem.repository.CustomerRepository;
-import ca.mcgill.ecse321.hotelsystem.repository.EmployeeRepository;
-import ca.mcgill.ecse321.hotelsystem.repository.OwnerRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.sql.Date;
 import java.util.List;
 
+@Service
 public class AccountService {
 
     @Autowired
     AccountRepository accountRepository;
 
-    @Autowired
-    CustomerRepository customerRepository;
-
-    @Autowired
-    OwnerRepository ownerRepository;
-
-    @Autowired
-    EmployeeRepository employeeRepository;
-
+    /**
+     * GetAllAccounts: service method to fetch all existing accounts in the database
+     * @return List of accounts
+     */
     @Transactional
     public List<Account> getAllAccounts() {
         return accountRepository.findAll();
     }
 
+    /**
+     * GetAccountByAccountNumber: service number to fetch an existing account with a specific account number
+     * @param accountNumber: number of the account
+     * @return account
+     */
     @Transactional
     public Account getAccountByAccountNumber(int accountNumber) {
         Account account = accountRepository.findAccountByAccountNumber(accountNumber);
         return account;
     }
 
+    /**
+     * CreateAccount: service method to create and store an account in the database
+     * @param password: password for the account
+     * @param address: address for the account
+     * @param dob: date of birth for the account
+     * @return created account
+     */
     @Transactional
     public Account createAccount(String password, String address, Date dob){
         Account account = new Account(password, address, dob);
--- hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/service/OwnerService.java
+++ hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/service/OwnerService.java
@@ -1,4 +1,25 @@
 package ca.mcgill.ecse321.hotelsystem.service;
 
+import ca.mcgill.ecse321.hotelsystem.Model.Owner;
+import ca.mcgill.ecse321.hotelsystem.repository.AccountRepository;
+import ca.mcgill.ecse321.hotelsystem.repository.OwnerRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OwnerService {
+
+    @Autowired
+    OwnerRepository ownerRepository;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Transactional
+    public Owner getOwnerByEmail(String email){
+        Owner owner = ownerRepository.findOwnerByEmail(email);
+        return owner;
+    }
+
 }
