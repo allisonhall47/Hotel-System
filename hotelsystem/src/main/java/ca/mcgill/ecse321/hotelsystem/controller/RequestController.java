c5ebe71e9c902aea5d079143ee84f7f707de7504
PatrickRic <patrick.ric02@gmail.com>
2023-11-05 21:51:38 -0500

    started request integration tests

--- hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/controller/RequestController.java
+++ hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/controller/RequestController.java
@@ -6,6 +6,7 @@ import ca.mcgill.ecse321.hotelsystem.dto.RequestRequestDto;
 import ca.mcgill.ecse321.hotelsystem.dto.RequestResponseDto;
 import ca.mcgill.ecse321.hotelsystem.service.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,38 +18,45 @@ public class RequestController {
     private RequestService service;
 
     @GetMapping ("/request")
+    @ResponseStatus(HttpStatus.OK)
     public List<RequestResponseDto> getAllRequests() {
         return service.getAllRequests().stream().map(req -> new RequestResponseDto(req)).toList();
     }
 
     @GetMapping ("/request/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public RequestResponseDto getRequestWithId(@PathVariable int id) {
         return new RequestResponseDto(service.getRequest(id));
     }
 
     @GetMapping ("request/reservation/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public List<RequestResponseDto> getRequestsForReservation(@PathVariable int id) {
         return service.getRequestsForReservationWithId(id).stream().map(req -> new RequestResponseDto(req)).toList();
     }
 
     @PostMapping ("request/new")
+    @ResponseStatus(HttpStatus.CREATED)
     public RequestResponseDto createRequest(@RequestBody  RequestRequestDto req) {
         Request request = service.createRequest(req.getDescription(), req.getReservationId());
         return new RequestResponseDto(request);
     }
 
-    @PatchMapping ("request/{id}") //TODO: Completion status in requestBody? (make dto?)
+    @PatchMapping ("request/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public RequestResponseDto changeRequestStatus(@PathVariable int id, @RequestBody CompletionStatus status) {
         return new RequestResponseDto(service.changeRequestStatus(id, status));
     }
 
     @DeleteMapping("request/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public String deleteRequest(@PathVariable int id) {
         service.deleteRequest(id);
         return "redirect:/request"; //TODO: review
     }
 
     @DeleteMapping("request/reservation/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public String deleteRequestsForReservation(@PathVariable int id) {
         service.deleteRequestsForReservationWithId(id);
         return "redirect:/reservation/" + id; //TODO: review
--- hotelsystem/src/test/java/ca/mcgill/ecse321/hotelsystem/integration/RepairIntegrationTests.java
+++ hotelsystem/src/test/java/ca/mcgill/ecse321/hotelsystem/integration/RepairIntegrationTests.java
@@ -6,9 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import ca.mcgill.ecse321.hotelsystem.repository.EmployeeRepository;
 import ca.mcgill.ecse321.hotelsystem.repository.RepairRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -17,6 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RepairIntegrationTests {
 
     @Autowired
@@ -28,56 +28,17 @@ public class RepairIntegrationTests {
     @Autowired
     private EmployeeRepository employeeRepo;
 
-    @BeforeEach
-    @AfterEach
+    @BeforeAll
+    @AfterAll
     public void clearDatabase() {
         repairRepo.deleteAll();
         employeeRepo.deleteAll();
     }
 
     @Test
-    public void testCreateAndGetRepair() {
-        ResponseEntity<> emp = client.postForEntity("/employee")
-        testGetPerson(id);
-    }
+    @Order(1)
+    public void testCreateRepair() {
 
-    private int testCreateRepair(int employeeId) {
-        ResponseEntity<PersonDto> response = client.postForEntity("/person", new PersonDto("Obi-Wan Kenobi"), PersonDto.class);
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.CREATED, response.getStatusCode(), "Response has correct status");
-        assertNotNull(response.getBody(), "Response has body");
-        assertEquals("Obi-Wan Kenobi", response.getBody().getName(), "Response has correct name");
-        assertTrue(response.getBody().getId() > 0, "Response has valid ID");
-
-        return response.getBody().getId();
-    }
-
-    private void testGetPerson(int id) {
-        ResponseEntity<PersonDto> response = client.getForEntity("/person/" + id, PersonDto.class);
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK, response.getStatusCode(), "Response has correct status");
-        assertNotNull(response.getBody(), "Response has body");
-        assertEquals("Obi-Wan Kenobi", response.getBody().getName(), "Response has correct name");
-        assertTrue(response.getBody().getId() == id, "Response has correct ID");
-    }
-
-    @Test
-    public void testCreateInvalidPerson() {
-        ResponseEntity<String> response = client.postForEntity("/person", new PersonDto("   "), String.class);
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode(), "Response has correct status");
-    }
-
-    @Test
-    public void testGetInvalidPerson() {
-        ResponseEntity<String> response = client.getForEntity("/person/" + Integer.MAX_VALUE, String.class);
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode(), "Response has correct status");
-        assertEquals("Person not found.", response.getBody(), "Response has correct error message");
     }
 }
 
