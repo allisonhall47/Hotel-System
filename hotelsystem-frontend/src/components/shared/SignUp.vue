dadc0b030066fbefe3b3dd5fc577b0e1fdabac35
allisonhall47 <allison.hall@mail.mcgill.ca>
2023-11-20 14:44:28 -0500

    comment out axios

--- hotelsystem-frontend/src/components/shared/SignUp.vue
+++ hotelsystem-frontend/src/components/shared/SignUp.vue
@@ -78,14 +78,14 @@
 
 <script>
 
-import axios from 'axios'
-var config = require('../../../config')
-var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
-var axiosClient = axios.create({
-  baseURL: backendUrl,
-  headers: { 'Access-Control-Allow-Origin': frontendUrl }
-})
+// import axios from 'axios'
+// var config = require('../../../config')
+// var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+// var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+// var axiosClient = axios.create({
+//   baseURL: backendUrl,
+//   headers: { 'Access-Control-Allow-Origin': frontendUrl }
+// })
 
 export default {
   name: 'SignUp',
