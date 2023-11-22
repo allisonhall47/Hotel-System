a296b18c4605674c9f6af1f941d09a3eff644351
wasifsomji <wasifsomji@gmail.com>
2023-11-22 13:54:41 -0500

    set up pages

---
+++ hotelsystem-frontend/src/components/employee/EmployeeHome.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div id = "main"></div>
+    <div class="hero-section">
+      <div class="navbar-container">
+        <nav class="navbar navbar-expand-lg navbar-light transparent-background">
+          <a class="navbar-brand" href="#">
+            <img src="../../assets/marwaniottNoBG.png" alt="Your Logo" height="60">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" @click="Login">LogIn</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" @click="SignUp">SignUp</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      <!--start rest of page here-->
+
+    </div>
+  </div>
+</template>
---
+++ hotelsystem-frontend/src/components/employee/EmployeeRepair.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "EmployeeRepair"
+}
+</script>
+
+<style scoped>
+
+</style>
