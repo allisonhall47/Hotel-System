bee99ee223a5e89b41be3fbeb2a3f0d6246db863
allisonhall47 <allison.hall@mail.mcgill.ca>
2023-11-25 17:13:09 -0500

    reconfigure login and signup

--- hotelsystem-frontend/src/components/customer/CustomerHome.vue
+++ hotelsystem-frontend/src/components/customer/CustomerHome.vue
@@ -33,7 +33,7 @@
             <div class="col-md-12 mx-auto text-center">
               <h1 class="text-center" style="font-family: 'Montserrat', serif; color: #888; letter-spacing: 5px; font-size: 45px" >THE MARWANIOTT HOTEL</h1>
               <p class="luxurious-text" style="font-weight: normal; color: #888;">explore. relax. rejuvenate.</p>
-              <button class="btn btn-lg mt-3 custom-login-button" @click="showLoginModal">Book Your Stay</button>
+              <button class="btn btn-lg mt-3 custom-login-button" @click="">Book Your Stay</button>
             </div>
           </div>
         </div>
@@ -110,9 +110,6 @@ export default {
     async LogOut() {
       alert('Successfully logged out.')
       await this.$router.push({name: 'Home'})
-    },
-    showLoginModal() {
-      console.log('Show login modal');
     },
   }
 };
