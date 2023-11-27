d9ea995ed8ba014479a24b1cbcabafa2ee738902
allisonhall47 <allison.hall@mail.mcgill.ca>
2023-11-26 23:47:29 -0500

    remove premade

--- hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/HotelsystemApplication.java
+++ hotelsystem/src/main/java/ca/mcgill/ecse321/hotelsystem/HotelsystemApplication.java
@@ -1,9 +1,7 @@
 package ca.mcgill.ecse321.hotelsystem;
 
 import ca.mcgill.ecse321.hotelsystem.Model.*;
-import ca.mcgill.ecse321.hotelsystem.repository.CustomerRepository;
 import ca.mcgill.ecse321.hotelsystem.repository.OwnerRepository;
-import ca.mcgill.ecse321.hotelsystem.repository.EmployeeRepository;
 import ca.mcgill.ecse321.hotelsystem.service.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,21 +16,11 @@ public class HotelsystemApplication {
 
 	// created mock owner and employee for login
 	@Bean
-	CommandLineRunner initDatabase(@Autowired OwnerService ownerService, @Autowired RoomService roomService, @Autowired SpecificRoomService specificRoomService, @Autowired OwnerRepository ownerRepository, @Autowired AccountService accountService, @Autowired EmployeeService employeeService, @Autowired EmployeeRepository employeeRepository, @Autowired CustomerService customerService, @Autowired CustomerRepository customerRepository, @Autowired ReservationService reservationService, @Autowired ReservedRoomService reservedRoomService){
+	CommandLineRunner initDatabase(@Autowired OwnerService ownerService, @Autowired RoomService roomService, @Autowired SpecificRoomService specificRoomService, @Autowired OwnerRepository ownerRepository, @Autowired AccountService accountService){
 		return args -> {
 			if (ownerRepository.count() == 0){
 				Account a = accountService.createAccount(new Account("OwnerAccount123", "The Marwaniott", LocalDate.of(1980, 1, 1)));
 				ownerService.createOwner(new Owner("marwan@themarwaniott.com", "Marwan", a));
-				Account b = accountService.createAccount(new Account("EmployeeAccount123","The Marwaniott", LocalDate.of(1980,1,1)));
-				employeeService.createEmployee(new Employee("tony@themarwaniott.com", "Tony", 6000, b));
-				Account c = accountService.createAccount(new Account("PasswordAccount123", "The Marwaniott", LocalDate.of(1980,1,1)));
-				Customer customer = new Customer("anniegouchee@gmail.com","Annie", c);
-				customerService.createCustomer(customer);
-				Reservation res1 = reservationService.createReservation(new Reservation(4, LocalDate.of(1990,3,3), LocalDate.of(1990,3,6), 0, false, CheckInStatus.BeforeCheckIn, customer));
-//				reservedRoomService.createReservedRoom(new ReservedRoom(res1, specificRoom1));
-				//customerService.createCustomer(new Customer("anniegouchee@gmail.com","Annie", c));
-				Account d = accountService.createAccount(new Account("EmployeeAccount123", "The Marwaniott", LocalDate.of(1990,2,2)));
-				employeeService.createEmployee(new Employee("louis@themarwaniott.com","Louis", 8000, d));
 
 				Room regRoom = roomService.createRoom(new Room("Regular", 450, BedType.Queen, 2));
 				Room deleuxeRoom = roomService.createRoom(new Room("Deluxe", 600, BedType.Queen, 4));
